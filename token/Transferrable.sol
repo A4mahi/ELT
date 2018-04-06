c59403625d15655570d0183da6ca177e2767f4fa
George McIntyre <george@lendo.io>
2018-04-07 01:21:48 +0200

    Remove superluous import declare event and emit it in the right place

--- token/Transferrable.sol
+++ token/Transferrable.sol
@@ -6,16 +6,17 @@ pragma solidity ^0.4.21;
  * Implementation for Basic Token:
  *   ERC20 and Time Vault functions
  */
-import "../lendo/TimeVaultInterface.sol";
 import "./ERC20Token.sol";
 import "../util/Owned.sol";
 
 contract Transferrable is owned, ERC20Token {
     
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
     function transferOwnership(address newOwner) onlyOwner public {
         transferByOwner(newOwner, balanceOf(newOwner), 0);
         owner = newOwner;
-        OwnershipTransferred(msg.sender, newOwner);
+        emit OwnershipTransferred(msg.sender, newOwner);
     }
 
 }
--- util/Owned.sol
+++ util/Owned.sol
@@ -1,16 +1,11 @@
 pragma solidity ^0.4.21;
 
-// import "../lendo/TimeVaultInterface.sol";
-// import "../ERC223/ERC223Interface.sol";
-
 /**
  * @title Owned
  * @dev To verify ownership
  */
 contract owned {
     address public owner;
-
-    event OwnershipTransferred(address indexed _from, address indexed _to);
 
     function owned() public {
         owner = msg.sender;
